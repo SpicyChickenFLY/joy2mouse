82832b270308e2d45bd3d9432a668ab204ecb91f
SpicyChickenFLY <xixiayuchao@gmail.com>
2021-04-15 10:36:29 +0800

    improve keyboardSimulator funcs

--- keyboard/keyboard.go
+++ keyboard/keyboard.go
@@ -35,10 +35,15 @@ func (ks *KeyboardSimulator) InitKB(xg *xgc.XinputGamepad) (err error) {
 }
 
 func (ks *KeyboardSimulator) Handle(xg *xgc.XinputGamepad) error {
-	ltPulled := false
+	var ltPulled bool
+	var alphabetDict *[][]string
 	// First judge if Left Trigger pulled
-	if xg.JudgeLTPulled {
+	if xg.JudgeLTPulled() {
 		ltPulled = true
+		alphabetDict = &alphabetHigherCaseDict
+	} else {
+		ltPulled = false
+		alphabetDict = &alphabetLoweraseDict
 	}
 	// if Right Trigger pulled, judge which key should be simulated
 	if xg.JudgeRTPulled() {
@@ -46,31 +51,36 @@ func (ks *KeyboardSimulator) Handle(xg *xgc.XinputGamepad) error {
 		if xg.JudgeThumbLPulled() {
 			lx := float64(xg.ThumbLX) / xgc.ThumbMax
 			ly := float64(xg.ThumbLY) / xgc.ThumbMax
-			ll := len(alphabetDict)
+			ll := len(*alphabetDict)
 			lSec = judgePosSection(lx, ly, ll)
 		}
 		if lSec != 0 {
 			if xg.JudgeThumbRPulled() {
 				rx := float64(xg.ThumbRX) / xgc.ThumbMax
 				ry := float64(xg.ThumbRY) / xgc.ThumbMax
-				rl := len(alphabetDict[lSec])
+				rl := len((*alphabetDict)[lSec])
 				rSec = judgePosSection(rx, ry, rl)
 			}
 		}
 		if rSec != 0 {
-			if ltPulled {
-				ks.kbEvent.HasSHIFT(true)
-				ks.kbEvent.AddKey()
-			}
+			keyVal := (*alphabetDict)[lSec][rSec]
+			ks.kbEvent.HasSHIFT(keyInfoMap[keyVal].Flag&kbEventHasShift > 0)
+			ks.kbEvent.AddKey(keyInfoMap[keyVal].VK)
 			err := ks.kbEvent.Launching()
 			if err != nil {
 				return err
 			}
 		}
-
 	}
 	// judge rest input in order and not aborted
+	if xg.Buttons&xgc.XinputGamepadDpad > 0 {
+		if ltPulled {
 
+		} else {
+
+		}
+	}
+	return nil
 }
 
 // func (ks *KeyboardSimulator)
