40edc9e5ac8ab3d461038707da6b713051f91827
SpicyChickenFLY <xixiayuchao@gmail.com>
2021-04-20 18:05:32 +0800

    try to fix degree calc

--- keyboard/utils.go
+++ keyboard/utils.go
@@ -13,14 +13,21 @@ import (
 // l: 代表该圆中存在几个扇形
 func judgePosSection(x, y float64, l int) int {
 	// FIXME: How to translate (x,y) to degree properly
-	temp := math.Atanh(y/x) * 180 / math.Pi
 	// 将极坐标正方向反转后逆时针旋转90度（求余用于溢出角度）
-	degree := int(-temp+90) % 360
+	degree := int(calculateDegree(x, y)+90) % 360
 	interval := 360.0 / l
-	utils.TBPrint(20, 0, 0, 0, fmt.Sprintf("%.2f/%.2f %.2f %3d/%3d", x, y, temp, degree, interval))
+	utils.TBPrint(20, 0, 0, 0, fmt.Sprintf("%.2f/%.2f %3d/%3d", x, y, degree, interval))
 	if degree/interval > l {
-		errStr := fmt.Sprintf("t:%f,l:%d,d:%d,i:%d-%d", temp, l, degree, interval, degree/interval)
+		errStr := fmt.Sprintf("l:%d,d:%d,i:%d-%d", l, degree, interval, degree/interval)
 		panic(errors.New(errStr))
 	}
 	return degree / interval
+}
+
+func calculateDegree(x, y float64) float64 {
+	if y > 0 {
+		return math.Acos(x / math.Sqrt(x*x+y*y))
+	} else {
+		return math.Pi*2 - math.Acos(x/math.Sqrt(x*x+y*y))
+	}
 }
